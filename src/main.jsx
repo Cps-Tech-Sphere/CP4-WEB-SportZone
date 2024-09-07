6f38465d6c8681b64949781ff71ec9f155cd9291
Jean <jeanmatheus0612@gmail.com>
2024-09-07 15:50:26 -0300

    Configurando o main

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,43 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Produtos from './routes/Produtos.jsx'
+import Sobre from './routes/Sobre.jsx'
+import Contato from './routes/Contato.jsx'
+import GlobalStyled from './global-styled.js'
+
+const router = createBrowserRouter(
+  [
+    {
+      path:'/',
+      element: <App/>,
+      children:[
+        {
+          path:'/',
+          element: <Home/>
+        },
+        {
+          path:'/produtos',
+          element: <Produtos/>
+        },
+        {
+          path:'/contato',
+          element: <Contato/>
+        },
+        {
+          path:'/sobre',
+          element: <Sobre/>
+        }
+      ]
+    }
+  ]
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <GlobalStyled/>
   </StrictMode>,
 )

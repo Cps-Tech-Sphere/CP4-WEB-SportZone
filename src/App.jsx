317a55eb707d44c2b1cc1039698775788ad46c54
Jean <jeanmatheus0612@gmail.com>
2024-09-07 15:49:40 -0300

    Configurando o App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import Footer from "./components/Footer"
+import Nav from "./components/Nav"
+import { CorpoSite } from "./styled"
+import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <CorpoSite>
+      <Nav/>
+      <Outlet/>
+      <Footer/>
+    </CorpoSite>
   )
 }
 

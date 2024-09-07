4678e9e7543e0c98e3a2e26514569f85a2f7f194
Jean <jeanmatheus0612@gmail.com>
2024-09-07 15:48:44 -0300

    Criando a rota Sobre

---
+++ src/routes/Sobre.jsx
@@ -0,0 +1,43 @@
+import { NavSobre } from "../styled"
+
+const Sobre =()=>{
+
+    return (
+        <NavSobre>
+            <div id="banner-sobre">
+                <h1>Banner</h1>
+            </div>
+
+            <h2 id="titulo-sobre">Sobre Nós</h2>
+
+            <div className="paragrafo-sobre">
+                <p id="parag1">
+                Bem-vindo à SportZone, a sua loja de confiança para artigos esportivos! Nossa missão é oferecer o melhor em equipamentos, vestuário e acessórios para todos os esportes e atividades físicas, desde os mais casuais até os de alta performance. Seja você um iniciante ou um atleta experiente, a SportZone tem tudo o que você precisa para alcançar o seu melhor desempenho.
+                </p>
+                <p>
+                Fundada com a paixão pelo esporte, a SportZone foi criada para tornar a prática esportiva acessível a todos. Nossa ampla gama de produtos abrange diversas modalidades, como futebol, basquete, ciclismo, natação, corrida, musculação, esportes de aventura e muito mais. Trabalhamos com as melhores marcas do mercado, sempre garantindo qualidade, conforto e inovação.
+                </p> 
+
+                <p>
+                Aqui, valorizamos o atendimento personalizado e a experiência do cliente. Nossa equipe de especialistas está sempre pronta para ajudar, oferecendo orientação e recomendações de acordo com suas necessidades e objetivos esportivos.
+                </p>
+
+                <p>
+                Além disso, acreditamos no poder transformador do esporte como uma ferramenta de inclusão e bem-estar. Por isso, promovemos eventos, workshops e iniciativas que incentivam a prática esportiva em diversas comunidades. Nossa meta é não apenas equipar você com os melhores produtos, mas também inspirar um estilo de vida saudável e ativo para todos.
+                </p>
+
+                <p>
+                Na SportZone, acreditamos que o esporte transforma vidas, e estamos comprometidos em inspirar e equipar você para superar seus limites, viver de forma saudável e ativa, e desfrutar da prática esportiva em todas as suas formas. 
+                </p>
+
+                <p>
+                Explore o nosso site, visite nossas lojas físicas ou faça parte da nossa comunidade online. Venha descobrir o mundo do esporte com a SportZone! 
+                </p> 
+
+            </div>
+
+        </NavSobre>
+    )
+}
+
+export default Sobre
